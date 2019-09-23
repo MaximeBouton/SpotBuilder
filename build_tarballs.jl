8fe1b85798e2f72faeb73c657f67af390333f055
SISL <sisl@MacBook-Pro-3.local>
2019-09-23 14:21:18 -0700

    more gcc versions

--- build_tarballs.jl
+++ build_tarballs.jl
@@ -17,8 +17,6 @@ cd $WORKSPACE/srcdir
 cd spot-2.8.1
 
 if [[ "${target}" == *-freebsd* ]] || [[ "${target}" == *-apple-* ]]; then
-    export CC=/opt/${target}/bin/${target}-gcc
-    export CXX=/opt/${target}/bin/${target}-g++
     export FC=/opt/${target}/bin/${target}-gfortran
     export LD=/opt/${target}/bin/${target}-ld
     export AR=/opt/${target}/bin/${target}-ar
@@ -36,8 +34,10 @@ make install
 # platforms are passed in on the command line
 platforms = [
     Windows(:x86_64, compiler_abi=CompilerABI(:gcc7)),
+    Windows(:x86_64, compiler_abi=CompilerABI(:gcc8)),
     Linux(:x86_64, compiler_abi=CompilerABI(:gcc7)),
-    MacOS(:x86_64, compiler_abi=CompilerABI(:gcc7))
+    Linux(:x86_64, compiler_abi=CompilerABI(:gcc8)),
+    MacOS(:x86_64)
 ]
 
 # The products that we will ensure are always built
